481bb8cf4ba935e5066662e6a8bd77ab05a237ac
Aliya Mullina <aliya.mullina@ya.ru>
2021-07-19 20:28:54 +0300

    Правки в print

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ def game() -> None:
             hero_attack = swords(hero_attack)
         else:
             hero_hp, hero_attack, m_counter = monsters(hero_hp, hero_attack, m_counter)
-            print(f"Твой счет ", str(m_counter), " из ", str(win_count))
+            print(f"СЧЕТ! ", str(m_counter), " из ", str(win_count))
 
         if m_counter == win_count:
             print("ПОБЕДА")
@@ -62,25 +62,25 @@ def monsters(x: int, y: int, z: int) -> Tuple[int, int, int]:
 
     # Сражение с чудовищем:
     print(
-        f"На горизонте чудовище с ",
+        f"ЧУДОВИЩЕ! Жизнь ",
         str(monster_hp),
-        " жизнями и с силой атаки ",
+        "и сила атаки ",
         str(monster_attack),
-        ". У тебя жизнь: ",
+        ". Твоя жизнь ",
         str(x),
-        " и сила атаки: ",
+        " и сила атаки ",
         str(y),
     )
 
     # Действия
-    print("БОЙ! 1 - сражаться, 2 - убежать, чтобы набраться сил")
+    print("БОЙ! Выбери 1 - сражаться, 2 - убежать, чтобы набраться сил:")
     answer = get_input()
     if answer in "1":
         print("СРАЖЕНИЕ!")
         if y >= monster_hp and x > monster_attack:
             x = x - monster_attack
             z = z + 1
-            print(f"УСПЕХ! В бою твоя жизнь сократилась до ", str(x))
+            print(f"УСПЕХ! Твоя жизнь сократилась до ", str(x))
             return x, y, z
         else:
             x = 0
@@ -102,19 +102,19 @@ def swords(x: int) -> int:
 
     # Действия
     print(
-        f"Найден МЕЧ с силой атаки ",
+        f"МЕЧ, новый -",
         str(sword_attack),
-        "Старый ",
+        "старый -",
         str(x),
-        ". 1 - взять себе, 2 - пройти мимо",
+        ". Выбери 1 - взять себе, 2 - пройти мимо:",
     )
     answer = get_input()
     if answer in "1":
         x = sword_attack
-        print(f"Новая сила атаки: ", str(x))
+        print(f"Сила атаки - ", str(x))
         return x
     else:
-        print("Проходишь мимо...")
+        print("Проходишь мимо")
         return x
 
 
@@ -127,7 +127,7 @@ def apples(x: int) -> int:
     """
     apple_hp = random.randint(3, 5)
     x = x + apple_hp
-    print(f"ЯБЛОЧКО! Количество жизней увеличилось на ", str(apple_hp), " и равно ", str(x))
+    print(f"ЯБЛОЧКО! Твоя жизнь увеличилась на ", str(apple_hp), " и равно ", str(x))
     return x
 
 
@@ -141,7 +141,7 @@ def get_input() -> str:
         answer = input()
         if answer in ("1", "2"):
             return answer
-        print("Действие не распознано. Введи еще раз 1 или 2")
+        print("Действие не распознано. Введи еще раз 1 или 2:")
 
 
 if __name__ == "__main__":
